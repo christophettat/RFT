615852e266c1c0145e7dee8db33c9fc1f7d30511
christophe bolle <christophe_xxx@yahoo.fr>
2019-04-26 15:05:25 +0200

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RFT
+a python installer to setup a complete RobotFramework test environment with core, ide, libraries
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='rft',  
+     version='0.1',
+     scripts=['rft'] ,
+     author="Christophe Bolle",
+     author_email="christophe_xxx@yahoo.fr",
+     description="RFT is a collection of tools and libraries around RobotFramework",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/christophettat/RFT",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 2",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
